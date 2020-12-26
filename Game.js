11d576342159300f78a0b812843aff67655340b6
srujan-758 <73021947+srujan-758@users.noreply.github.com>
2020-12-26 15:18:00 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,52 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+
+  play(){
+    form.hide();
+    
+    textSize(30);
+    text("Car Racing Game", 120, 100);
+
+    Player.getPlayerInfo();
+
+    if(allPlayers!==undefined){
+      var displayPosition = 130;
+      for(var plr in allPlayers){
+        
+        displayPosition+=20
+        if(plr =="player"+player.index)
+          fill("red");
+        else
+          fill("black");
+        textSize(15);
+        text(allPlayers[plr].name+":"+allPlayers[plr].distance, 120, displayPosition)
+      }
+      
+    }
+    
+  }
+}
